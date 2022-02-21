36a2a5d4b4b62d9e4572a6a26e81080df5a4e51e
Mathias Weber Bjerge Nielsen <math319t@elev.tec.dk>
2022-02-21 15:29:56 +0100

    YES

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,15 +13,35 @@ namespace Eksamnensprojekt_2022
 {
     public partial class StartScreenForm : Form
     {
-        public class CsvReader
-        {
-
-        }
         public StartScreenForm()
         {
             InitializeComponent();
 
-            using (CsvReader csv = new CsvReader(new StreamReader("fil"), true);
+            string[] lines = File.ReadAllLines(@"c:\temp\arrayfil.csv");
+
+            List<string> sInd = new List<string>();
+
+            StringBuilder sb = new StringBuilder(lines[0]);
+            for (int j = 0; j < lines.Length; j++)
+            {
+                if (j % 3 == 0)
+                    sb.Replace(';', ':');
+
+            }
+
+            for (int i = 0; i < lines.GetLength(0); i++)
+            {
+                string[] fields = lines[i].Split(new char[] { ';' });
+
+                foreach (string item in fields)
+                {
+                    sInd.Add(item);
+                }
+            }
+
+            string[] sltArray = sInd.ToArray();
+
+
         }
     }
 }
